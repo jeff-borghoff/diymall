5f07a0965f56034f43b34a814d08d7051fff6a9c
Jeff Borghoff <55985071+jeff-borghoff@users.noreply.github.com>
2021-08-10 18:26:03 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,19 @@
+radio.onReceivedValue(function (name, value) {
+    if (name == "level") {
+        OLED12864_I2C.clear()
+        level = value
+    }
+})
+let level = 0
+radio.setGroup(1)
 OLED12864_I2C.init(60)
+OLED12864_I2C.zoom(true)
 OLED12864_I2C.on()
 basic.forever(function () {
-    OLED12864_I2C.showString(
-    0,
-    0,
-    "Hello!",
+    OLED12864_I2C.showNumber(
+    5,
+    2,
+    level,
     1
     )
 })
